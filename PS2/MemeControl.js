aa46df4da35580ee07f95a1c33a6d8d8dff9a8a6
Andres <andresf.mon@gmail.com>
2013-01-28 19:14:56 -0700

    initial

--- PS2/MemeControl.js
+++ PS2/MemeControl.js
@@ -1,5 +1,36 @@
-function CheckCaptionLength(text){
-	if (text.length >= 20){
-		
+function validate(){
+	//ensure the form is correct before submitting
+	var errMsg = "";
+	
+	var invalidText = false;
+	//loop through the textboxes for invalid input
+	$('input[name^="caption"]').each(function (){
+		var value = $.trim($(this).val());
+		if(value == '' || value.length > 20){
+			$(this).css('border', '3px #FF0000 solid');
+			invalidText = true;
+		}
+	});
+	if (invalidText)
+		errMsg += "\t*A textbox is either empty, or contains too \n\tmany characters.\n\t";
+	
+	//loop through the radio groups for invalid input
+	var invalidRadio = false;
+	$("input:radio").each(function(){
+		var name = $(this).attr("name");
+		if($("input:radio[name="+name+"]:checked").length == 0){
+			$(this).parent().css('border', '3px #FF0000 solid');
+			invalidRadio = true;
+		}
+	});
+	if(invalidRadio)
+		errMsg += "*A radio group is not selected.\n\t";			
+
+	//if there are no error the form is submittd otherwise there is an error alert displayed
+	if(errMsg == 0){
+		$("form").submit();
+	}
+	else{
+		alert("There is an error in the form:\n"+errMsg);
 	}
 }
--- PS2/YourMeme.php
+++ PS2/YourMeme.php
@@ -1,27 +1,17 @@
-<?php
-$image_markup = "<img src=$imagepath />";
-$imagepath = $_GET['pic'];
-$image=imagecreatefromjpeg($imagepath);
-header('Content-type: image/jpeg');
-
-imagejpeg($image);
-?>
-
 <html>
-<head>
+<head>	
+<link rel="stylesheet" href="MemeStyle.css" type="text/css" />
 </head>
 <body>
-<div class=pictureBase >
-	<table>
-		<tr>
-			<td><?php echo $image_markup  ?></td>
-			<td><img src=<?php echo $image ?> /></td>
-		</tr>
-		<tr>
-			<td><img src=<?php echo $image ?> /></td>
-			<td><img src=<?php echo $image ?> /></td>
-		</tr>
-	</table>
-</div>
+<table class=memePics>
+	<tr>
+		<td background=<?php echo $_GET['pic1']?> style=color:<?php echo $_GET['textColor1']?>><?php echo $_GET['caption1']?></td>
+		<td background=<?php echo $_GET['pic2']?> style=color:<?php echo $_GET['textColor2']?>><?php echo $_GET['caption2']?></td>
+	</tr>
+	<tr>
+		<td background=<?php echo $_GET['pic3']?> style=color:<?php echo $_GET['textColor3']?>><?php echo $_GET['caption3']?></td>
+		<td background=<?php echo $_GET['pic4']?> style=color:<?php echo $_GET['textColor4']?>><?php echo $_GET['caption4']?></td>
+	</tr>
+</table>
 </body>
 </html>
