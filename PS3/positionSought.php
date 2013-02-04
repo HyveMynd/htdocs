0d04b9694b9057c86beba06059c1001fcf294778
Andres <andresf.mon@gmail.com>
2013-02-04 09:30:56 -0700

    added PS3

---
+++ PS3/positionSought.php
@@ -0,0 +1,26 @@
+<?php 
+session_start();
+
+//store or retrieve session variables
+if(!isset($_SESSION["position"]))
+	$_SESSION["position"] = $_POST['position'];
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" href="style.css" type="text/css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+<title>Position Sought</title>
+</head>
+<body>
+	<form action="positionSought.php" method="post">
+	<h1>Position Sought</h1>
+	<h5>Please enter a description of the type of job you are in search for:</h5>
+		<textarea name=position rows=5 cols=50 autofocus required><?php echo $_SESSION['position']?></textarea>
+	<input type="submit" />
+	</form>
+	<p><a href="contactInfo.php" >Contact Info</a></p>
+	<p><a href="employmentHistory.php" >Employment History</a></p>
+</body>
+</html>
